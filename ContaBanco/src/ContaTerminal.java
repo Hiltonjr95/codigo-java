5ff07c4f975f3c0ea8562eb40eb91e17da0df02b
Hilton Júnior <hiltondev95@gmail.com>
2024-11-17 16:27:55 -0300

    teste

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
-public class App {
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite a agencia da conta");
+        agencia = scanner.nextLine();
+
+        System.out.println("Digite o nome do dono da conta");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite saldo da conta");
+        saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println(numero + agencia + nomeCliente + saldo);
+
+        scanner.close();
+
+
+
+        
     }
 }
